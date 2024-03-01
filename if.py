00b79ebe9c04c83ea0cb84940b0cd16ba21ab512
Benjaa :) <161756465+Benjaass@users.noreply.github.com>
2024-03-01 10:23:15 -0300

    Create if.py

---
+++ if.py
@@ -0,0 +1,7 @@
+## Crea un programa que simule la entrada a un boliche. debe dejar ingresar la edad del usuario. si es mayor a 18 mostrar por pantalla "puede ingresar", de lo contrario mostrar por pantalla "no puede ingresar"
+edad = int(input("ingrese su edad: "))
+
+if edad >= 18:
+    print("puede entrar :)")
+else:
+    print("no puede entrar :(")
